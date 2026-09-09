76e4a2bbc9ebd355adf053a98d56b479bcba2019
Jihoo <jihuu621@gmail.com>
2026-09-10 01:16:04 +0900

    체력띠

--- Assets/Scripts/AI/CheshireCat/CheshireCatHealth.cs
+++ Assets/Scripts/AI/CheshireCat/CheshireCatHealth.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [RequireComponent(typeof(CheshireCatAI), typeof(EnemyDataManager))]
 public class CheshireCatHealth : MonoBehaviour, IDamageable
 {
-    [SerializeField, Min(1f)] private float maxHP = 800f;
+    [SerializeField, Min(1f)] private float maxHP = 600f;
 
     public float CurrentHP { get; private set; }
     public float MaxHP => maxHP;
